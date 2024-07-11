c3adacdaf1e8628b8a1d421598dac60df7df9ea4
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-07-11 20:45:19 +0200

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <h1 style={{ fontSize: "30px", marginLeft: "50px" }}>ShoppingCart ©</h1>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
