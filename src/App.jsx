f5352551f718e4c1d7a71c73c96dcbfe43d4f37b
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-07-11 20:47:11 +0200

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Outlet from "./pages/Outlet/Outlet";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+      <main className="main-app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/outlet" element={<Outlet />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
