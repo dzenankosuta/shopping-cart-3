4ab8a17cd549f08c78cd0b618608d35a411b0ea0
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-07-22 20:12:54 +0200

    add formattedPrice util

---
+++ src/utils/formattedPrice.js
@@ -0,0 +1,6 @@
+export const formattedPrice = (number) => {
+  return new Intl.NumberFormat("de-DE", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(number.toFixed(2));
+};
