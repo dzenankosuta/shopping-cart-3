46e10fee32da94a617c8da673615cf4cb6c37562
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-07-22 20:12:39 +0200

    CartCard new styles and disable buttons when it is needed

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { formattedPrice } from "../../utils/formattedPrice";
 import "./CartCard.css";
 export default function CartCard({
   image,
@@ -11,6 +12,9 @@ export default function CartCard({
   decrementProduct,
   incrementProduct,
 }) {
+  const strPrice = price;
+  const numPrice = +strPrice.replace(".", "").replace(",", ".");
+  const totalPrice = numPrice * quantity;
   return (
     <div className="product-cart">
       <img className="product-image" src={image} />
@@ -18,13 +22,17 @@ export default function CartCard({
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
-      <div className="">
-        <button onClick={decrementProduct}>-</button>
-        <p>{quantity}</p>
-        <button onClick={incrementProduct}>+</button>
+      <div className="quantity">
+        <p>Quantity: {quantity}</p>
+        <button onClick={incrementProduct} disabled={stock === 0}>
+          +
+        </button>
+        <button onClick={decrementProduct} disabled={quantity === 1}>
+          -
+        </button>
       </div>
       <div className="product-price">
-        <h2>{price}</h2>
+        <h2>{formattedPrice(totalPrice)}</h2>
         <p>In stock: {stock}</p>
       </div>
       <button onClick={onClick} className="remove-button">
